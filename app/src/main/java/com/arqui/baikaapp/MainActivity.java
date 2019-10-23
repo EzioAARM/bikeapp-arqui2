45a40daea6d63d678c35bc9f02d07651e534bbaf
Axel Rodriguez <alejandrom9712@gmail.com>
2019-10-23 17:27:38 -0600

    diseño

--- app/src/main/java/com/arqui/baikaapp/MainActivity.java
+++ app/src/main/java/com/arqui/baikaapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.arqui.baikaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,6 +12,7 @@ import android.view.MenuItem;
 public class MainActivity extends AppCompatActivity {
 
     Toolbar toolbar;
+    public static Double Circunferencia = 1.25; // Circunferencia de una rueda de bicicleta en metros
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +20,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         toolbar = findViewById(R.id.toolbar_navigation_principal_activity);
-        toolbar.setTitle("Inicio");
+        toolbar.setTitle("Baikapp");
         setSupportActionBar(toolbar);
         toolbar.inflateMenu(R.menu.main_toolbar);
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        Fragment fragment = new PrincipalActivity();
+        transaction.replace(R.id.contenedor_principal_principal_activity, fragment);
+        toolbar.setTitle("Estadisticas");
+        transaction.addToBackStack(null).commit();
 
     }
 
@@ -37,9 +46,23 @@ public class MainActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             case R.id.settings:
                 // show settings activity
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                Fragment fragment = new SettingsFragment();
+                transaction.replace(R.id.contenedor_principal_principal_activity, fragment);
+                transaction.addToBackStack(null).commit();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        if(getFragmentManager().getBackStackEntryCount() > 0) {
+            getFragmentManager().popBackStack();
+        }
+        else
+            super.onBackPressed();
     }
 }
