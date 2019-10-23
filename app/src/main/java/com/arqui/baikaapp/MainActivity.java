f5e81956e357f6fbd5fad6dee1b0ff76f15ed17f
Axel Rodriguez <alejandrom9712@gmail.com>
2019-10-23 13:03:35 -0600

    toolbar para acceder a configuraciones

--- app/src/main/java/com/arqui/baikaapp/MainActivity.java
+++ app/src/main/java/com/arqui/baikaapp/MainActivity.java
@@ -1,14 +1,45 @@
 package com.arqui.baikaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar_navigation_principal_activity);
+        toolbar.setTitle("Inicio");
+        setSupportActionBar(toolbar);
+        toolbar.inflateMenu(R.menu.main_toolbar);
+
+    }
+
+    // Menu icons are inflated just as they were with actionbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.settings:
+                // show settings activity
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
